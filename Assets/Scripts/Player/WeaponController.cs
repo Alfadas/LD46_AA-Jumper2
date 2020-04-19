5d067393da96cd10f5212076d7160d08c11e3587
kruemelkeksfan <37933687+kruemelkeksfan@users.noreply.github.com>
2020-04-20 01:34:09 +0200

    Added Handgun

---
+++ Assets/Scripts/Player/WeaponController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [SerializeField] private Vector3 aimPosition = Vector3.zero;
+    [SerializeField] private Vector3 muzzle = Vector3.zero;
+    [Tooltip("Maximum Deviation from Point of Aim in cm at a Target Distance of 100m")]
+    [SerializeField] private float spread = 50;
+    [SerializeField] private int roundsPerMinute = 600;
+    [SerializeField] private int magazineCapacity = 1;
+    [SerializeField] private float reloadTime = 2;
+    [SerializeField] private float muzzleVelocity = 40.0f;
+    [SerializeField] private GameObject bulletPrefab = null;
+    [SerializeField] private AudioClip fireSound = null;
+    private Vector3 hipPosition = Vector3.zero;
+    private AudioSource audioSource = null;
+    private float timePerRound = 1.0f;
+    private float lastShot = 0.0f;
+    private int shotCount = 0;
+    private float reloadStarted = 0.0f;
+
+    private void Start()
+        {
+        timePerRound = 1.0f / (roundsPerMinute / 60.0f);
+        hipPosition = transform.localPosition;
+        audioSource = gameObject.GetComponent<AudioSource>();
+        }
+
+    private void Update()
+        {
+        // Debug.Log(shotCount + "/" + magazineCapacity + " " + (Time.time - reloadStarted) + "/" + reloadTime);
+
+        if(shotCount <= 0 && reloadStarted < 0)
+            {
+            reloadStarted = Time.time;
+            }
+
+        if(reloadStarted >= 0 && Time.time - reloadStarted >= reloadTime)
+            {
+            reloadStarted = -1;
+            shotCount = magazineCapacity;
+            }
+
+        if(Input.GetButton("Fire1") && (Time.time - lastShot) >= timePerRound && shotCount > 0)
+            {
+            lastShot = Time.time;
+
+            --shotCount;
+
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, transform.position + transform.rotation * Vector3.Scale(muzzle, transform.localScale), transform.rotation);
+            Vector3 deviation = (Random.insideUnitSphere * spread) / 10000.0f;
+            bullet.GetComponent<Rigidbody>().AddForce((bullet.transform.forward + deviation) * muzzleVelocity, ForceMode.VelocityChange);
+
+            audioSource.clip = fireSound;
+            audioSource.Play();
+            }
+
+        if(Input.GetButtonDown("Fire2"))
+            {
+            transform.localPosition = aimPosition;
+            }
+
+        if(Input.GetButtonUp("Fire2"))
+            {
+            transform.localPosition = hipPosition;
+            }
+        }
+}
