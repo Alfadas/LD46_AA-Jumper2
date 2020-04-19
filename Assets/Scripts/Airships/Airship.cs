d118de33f14e2071efb759d53425c387883e183c
Alfadas <alfadas.3@gmail.com>
2020-04-19 23:35:22 +0200

    Added Metal and Score system

--- Assets/Scripts/Airships/Airship.cs
+++ Assets/Scripts/Airships/Airship.cs
@@ -8,13 +8,22 @@ public class Airship : MonoBehaviour
     [HideInInspector] public int speed;
     [SerializeField] int maxHealth = 100;
     [SerializeField] int killPoint = -500;
+    [SerializeField] int metal = 5;
     int health;
     private int notWaiting = 1;
+    MetalManager metallManager;
+    EnemyList enemyList;
 
     private void Start()
     {
         speed = maxSpeed;
         health = maxHealth;
+    }
+
+    public void SetManagingObjects(MetalManager metallManager, EnemyList enemyList)
+    {
+        this.metallManager = metallManager;
+        this.enemyList = enemyList;
     }
     // Update is called once per frame
     void Update()
@@ -22,7 +31,7 @@ public class Airship : MonoBehaviour
         Move();
         if (transform.position.z < killPoint)
         {
-            DestroyShip();
+            DestroyShip(false);
         }
     }
 
@@ -36,12 +45,17 @@ public class Airship : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
-            DestroyShip();
+            DestroyShip(true);
         }
     }
 
-    private void DestroyShip()
+    private void DestroyShip(bool kill)
     {
+        if (kill)
+        {
+            metallManager.AddMetalAndScore(metal);
+        }
+        enemyList.RemoveEnemy(this);
         Object.Destroy(gameObject, 0.2f);
     }
 }
---
+++ Assets/Scripts/PlayerBase/MetalManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MetalManager : MonoBehaviour
+{
+    [SerializeField] Text scoreText;
+    [SerializeField] Text metalText;
+    [SerializeField] int startingMetal = 20;
+    [SerializeField] int gainIntervall = 5;
+    [SerializeField] int metalGain = 2;
+    int score;
+    int metal;
+
+    public int GetMetal()
+    {
+        return metal;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddMetalAndScore(int value)
+    {
+        metal += value;
+        score += value;
+    }
+
+    public void DeductMetal (int value)
+    {
+        metal -= value;
+    }
+
+    void Start()
+    {
+        StartCoroutine(GainIntervall());
+        metal = startingMetal;
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + score;
+        metalText.text = "Metal: " + metal;
+    }
+
+    IEnumerator GainIntervall()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(gainIntervall);
+            metal += metalGain;
+        }
+    }
+}
