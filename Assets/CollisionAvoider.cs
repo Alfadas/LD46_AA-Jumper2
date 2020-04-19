6865fbb6efad4d504b4b723ea7fe34aff2cd9642
Alfadas <alfadas.3@gmail.com>
2020-04-19 17:00:22 +0200

    added Collision Avoider

---
+++ Assets/CollisionAvoider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAvoider : MonoBehaviour
+{
+    Airship airship;
+    Airship collidingAirship;
+    // Start is called before the first frame update
+    void Start()
+    {
+        airship = GetComponentInParent<Airship>();
+    }
+
+    IEnumerator CheckSpeed()
+    {
+        while (true)
+        {
+            if (collidingAirship != null)
+            {
+                CorrectSpeed();
+            }
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    private void CorrectSpeed()
+    {
+        if (collidingAirship.speed <= airship.maxSpeed)
+        {
+            airship.speed = collidingAirship.speed;
+        }
+        else
+        {
+            airship.speed = airship.maxSpeed;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Airship newCollidingAirship = other.gameObject.GetComponentInParent<Airship>();
+        if(collidingAirship != null)
+        {
+            collidingAirship = newCollidingAirship;
+            CorrectSpeed();
+        }
+    }
+}
