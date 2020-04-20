794d2afcc04717ef7be84dd0e94706ac27f16e7b
Alfadas <alfadas.3@gmail.com>
2020-04-20 04:14:53 +0200

    impl Base

---
+++ Assets/Base.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Base : Building
+{
+    [SerializeField] Text text;
+    [SerializeField] Text scoreText;
+    [SerializeField] GameObject loseScreen;
+    [SerializeField] SpawnManager spawnManager;
+    [SerializeField] GameObject canvas;
+    [SerializeField] MetalManager metalManager;
+    protected override void DestroyBuilding()
+    {
+        base.DestroyBuilding();
+        spawnManager.StopAllCoroutines();
+        loseScreen.SetActive(true);
+        scoreText.text = "Score: " + metalManager.GetScore();
+    }
+
+    private void Update()
+    {
+        text.text = "Base: " + health;
+    }
+}
