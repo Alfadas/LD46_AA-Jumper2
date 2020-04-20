75dd51deaae09651ae6099100796ec0f67a9e38d
Alfadas <alfadas.3@gmail.com>
2020-04-20 20:43:14 +0200

    main to menu

---
+++ Assets/Scripts/UI/ExitController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitController : MonoBehaviour
+{
+    [SerializeField] PlayerController player;
+    [SerializeField] WeaponController weaponController;
+    [SerializeField] GameObject exitPanel;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            player.setMouseVisible(!exitPanel.activeSelf);
+            weaponController.BlockShooting(!exitPanel.activeSelf);
+            exitPanel.SetActive(!exitPanel.activeSelf);
+        }
+    }
+
+    public void BackToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
