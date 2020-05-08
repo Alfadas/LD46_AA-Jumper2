0da40744d8f4c615f985f63eca14e6e89a50b7a8
kruemelkeksfan <37933687+kruemelkeksfan@users.noreply.github.com>
2020-05-08 15:56:14 +0200

    Reformatted Player Code to Allman Indentation Style.

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -4,66 +4,66 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private float maxFlightTime = 6.0f;
-    [SerializeField] private float scanAheadDistance = 10.0f;
-    [SerializeField] private int damage = 10;
-    [SerializeField] private float explosionSize = 8.0f;
-    [SerializeField] private float explosionDuration = 0.2f;
-    [SerializeField] private Material explosionColor = null;
-    private float bulletFired = 0.0f;
-    private Vector3 lastPosition = Vector3.zero;
-    private float impactTime = -1.0f;
-    private Vector3 originalScale = Vector3.one;
+	[SerializeField] private float maxFlightTime = 6.0f;
+	[SerializeField] private float scanAheadDistance = 10.0f;
+	[SerializeField] private int damage = 10;
+	[SerializeField] private float explosionSize = 8.0f;
+	[SerializeField] private float explosionDuration = 0.2f;
+	[SerializeField] private Material explosionColor = null;
+	private float bulletFired = 0.0f;
+	private Vector3 lastPosition = Vector3.zero;
+	private float impactTime = -1.0f;
+	private Vector3 originalScale = Vector3.one;
 
-    private void Start()
-        {
-        bulletFired = Time.time;
-        lastPosition = transform.position;
-        }
+	private void Start()
+	{
+		bulletFired = Time.time;
+		lastPosition = transform.position;
+	}
 
-    private void Update()
-        {
-        if(Time.time - bulletFired >= maxFlightTime)
-            {
-            DestroyBullet();
-            }
+	private void Update()
+	{
+		if(Time.time - bulletFired >= maxFlightTime)
+		{
+			DestroyBullet();
+		}
 
-        if(impactTime >= 0)
-            {
-            transform.localScale = originalScale * (((Time.time - impactTime) / explosionDuration) * explosionSize);
+		if(impactTime >= 0)
+		{
+			transform.localScale = originalScale * (((Time.time - impactTime) / explosionDuration) * explosionSize);
 
-            if(Time.time - impactTime >= explosionDuration)
-                {
-                impactTime = -1.0f;
-                DestroyBullet();
-                }
-            }
+			if(Time.time - impactTime >= explosionDuration)
+			{
+				impactTime = -1.0f;
+				DestroyBullet();
+			}
+		}
 
-        if((transform.position - lastPosition).magnitude > scanAheadDistance)
-            {
-            RaycastHit hit;
-            if(Physics.Raycast(lastPosition, transform.forward, out hit, scanAheadDistance) && !hit.collider.isTrigger)
-                {
-                Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+		if((transform.position - lastPosition).magnitude > scanAheadDistance)
+		{
+			RaycastHit hit;
+			if(Physics.Raycast(lastPosition, transform.forward, out hit, scanAheadDistance) && !hit.collider.isTrigger)
+			{
+				Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
 
-                HitCollector target = hit.collider.GetComponent<HitCollector>();
-                target?.GetDamage(Mathf.CeilToInt(rigidbody.mass * rigidbody.velocity.magnitude * damage));
+				HitCollector target = hit.collider.GetComponent<HitCollector>();
+				target?.GetDamage(Mathf.CeilToInt(rigidbody.mass * rigidbody.velocity.magnitude * damage));
 
-                transform.position = hit.point;
-                rigidbody.velocity = Vector3.zero;
-                rigidbody.useGravity = false;
+				transform.position = hit.point;
+				rigidbody.velocity = Vector3.zero;
+				rigidbody.useGravity = false;
 
-                gameObject.GetComponent<MeshRenderer>().material = explosionColor;
-                originalScale = transform.localScale;
-                impactTime = Time.time;
-                }
-           
-            lastPosition = transform.position;
-            }
-        }
+				gameObject.GetComponent<MeshRenderer>().material = explosionColor;
+				originalScale = transform.localScale;
+				impactTime = Time.time;
+			}
 
-    private void DestroyBullet()
-        {
-        Object.Destroy(gameObject, 0.2f);
-        }
-    }
+			lastPosition = transform.position;
+		}
+	}
+
+	private void DestroyBullet()
+	{
+		Object.Destroy(gameObject, 0.2f);
+	}
+}
--- Assets/Scripts/Player/WeaponController.cs
+++ Assets/Scripts/Player/WeaponController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class WeaponController : MonoBehaviour
-	{
+{
 	[SerializeField] private Vector3 aimPosition = Vector3.zero;
 	[SerializeField] private Vector3 muzzle = Vector3.zero;
 	[Tooltip("Maximum Deviation from Point of Aim in cm at a Target Distance of 100m")]
@@ -37,32 +37,32 @@ public class WeaponController : MonoBehaviour
 	public bool safety = false;
 
 	private void Start()
-		{
+	{
 		originalRotation = transform.localRotation;
 		timePerRound = 1.0f / (roundsPerMinute / 60.0f);
 		hipPosition = transform.localPosition;
 		audioSource = gameObject.GetComponent<AudioSource>();
 		bulletCounter = GameObject.Find("BulletCounter").GetComponent<Text>();
-		}
+	}
 
 	private void Update()
-		{
+	{
 		// Auto Reload
 		if(shotCount <= 0 && reloadStarted < 0)
-			{
+		{
 			reloadStarted = Time.time;
-			}
+		}
 
 		// Finish Reload after reloadTime
 		if(reloadStarted >= 0 && Time.time - reloadStarted >= reloadTime)
-			{
+		{
 			reloadStarted = -1;
 			shotCount = magazineCapacity;
-			}
+		}
 
 		// Fire Weapon
 		if(fire && (fireModes[fireMode] == 0 || shotsFired < fireModes[fireMode]) && !safety && (Time.time - lastShot) >= timePerRound && shotCount > 0)
-			{
+		{
 			lastShot = Time.time;
 
 			--shotCount;
@@ -77,7 +77,7 @@ public class WeaponController : MonoBehaviour
 
 			audioSource.clip = fireSound;
 			audioSource.Play();
-			}
+		}
 
 		// Recenter Weapon
 		float recoilAngle = Quaternion.Angle(transform.localRotation, originalRotation);
@@ -85,65 +85,65 @@ public class WeaponController : MonoBehaviour
 
 		// Update Bullet Counter
 		if(reloadStarted < 0)
-			{
+		{
 			bulletCounter.text = shotCount + "/" + magazineCapacity;
 			bulletCounter.alignment = TextAnchor.LowerRight;
-			}
+		}
 		else
-			{
+		{
 			string text = "Reload";
 			int lettercount = (int)(((Time.time - reloadStarted) / reloadTime) * (text.Length + 1));   // Add a virtual Letter to let the full Word appear for longer than 1 Frame
 			text = text.Substring(0, Mathf.Clamp(lettercount, 0, text.Length));                         // Subtract virtual Letter
 
 			bulletCounter.text = text;
 			bulletCounter.alignment = TextAnchor.LowerLeft;
-			}
 		}
+	}
 
 	// TODO: Remove and replace by actual Animation
 	public void aim()
-		{
+	{
 		transform.localPosition = aimPosition;
-		}
+	}
 	public void unaim()
-		{
+	{
 		transform.localPosition = hipPosition;
-		}
+	}
 
 	public void pullTrigger()
-		{
+	{
 		fire = true;
 		shotsFired = 0;
-		}
+	}
 
 	public void releaseTrigger()
-		{
+	{
 		fire = false;
-		}
+	}
 
 	public void reload()
-		{
+	{
 		if(reloadStarted < 0)
-			{
+		{
 			reloadStarted = Time.time;
 			shotCount = 0;
-			}
-		}
-
-	public void switchFireMode(int fireMode = -1)
-		{
-		if(fireMode >= 0)
-			{
-			this.fireMode = fireMode;
-			}
-		else
-			{
-			this.fireMode = (this.fireMode + 1) % fireModes.Length;
-			}
-		}
-
-	public void toggleSafety(bool safety)
-		{
-		this.safety = safety;
 		}
 	}
+
+	public void switchFireMode(int fireMode = -1)
+	{
+		if(fireMode >= 0)
+		{
+			this.fireMode = fireMode;
+		}
+		else
+		{
+			this.fireMode = (this.fireMode + 1) % fireModes.Length;
+		}
+	}
+
+	public void toggleSafety(bool safety)
+	{
+		this.safety = safety;
+	}
+}
