b7e7d751384c2e1146c755224aa9cecc18909085
Alfadas <alfadas.3@gmail.com>
2020-05-08 17:30:09 +0200

    #24 moved PlayerLife to player scripts

---
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    [SerializeField] Transform respawnPoint;
+    [Header("Movement boundaries")]
+    [SerializeField] int zMax = 300;
+    [SerializeField] int zMin = -300;
+    [SerializeField] int yMax = 200;
+    [SerializeField] int yMin = -50;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Kill"))
+        {
+            Respawn();
+        }
+        if (transform.position.z > zMax)
+        {
+            Respawn();
+        }
+        if (transform.position.z < zMin)
+        {
+            Respawn();
+        }
+        if (transform.position.y < yMin)
+        {
+            Respawn();
+        }
+        if (transform.position.y > yMax)
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        transform.position = respawnPoint.position;
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero; // reset velocity to stop pre respawn movement
+    }
+}
