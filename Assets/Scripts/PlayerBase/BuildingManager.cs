3d3db2963db56048bf300c5288607fbe59aac626
Alfadas <alfadas.3@gmail.com>
2020-05-09 00:40:23 +0200

    #24 Refactored BuildingManager

--- Assets/Scripts/PlayerBase/BuildingManager.cs
+++ Assets/Scripts/PlayerBase/BuildingManager.cs
@@ -1,48 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class BuildingManager : MonoBehaviour
 {
-    [SerializeField] GameObject text;
+    [Tooltip("Panel to sigalize interactivity")]
+    [SerializeField] GameObject interactHelp;
+    [Tooltip("Building Menu Panel")]
     [SerializeField] BuildingPanelBuilder buildingUi;
     [SerializeField] PlayerController playerController;
+    [Tooltip("Controller of the player Weapon")]
     [SerializeField] WeaponController weaponController;
-    BuildingBase buildingBase;
+    BuildingBase buildingBase; //buildingBase used to interact with
 
     private void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 5))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 5)) //search for BuildingBases
         {
             buildingBase = hit.collider.gameObject.GetComponent<BuildingBase>();
-            if (buildingBase != null && !buildingBase.IsFilled)
+            if (buildingBase != null && !buildingBase.IsFilled) //if free buildingBase is in front of the player
             {
                 if (!buildingUi.isActiveAndEnabled)
                 {
-                    text.SetActive(true);
+                    interactHelp.SetActive(true);//show interact help only if there is a BuildingBase and no Menu shown
                     if (Input.GetButton("Interact"))
                     {
                         buildingUi.gameObject.SetActive(true);
                         buildingUi.SetupTurretPanels(this);
+                        //stop userinput to normal controlls to properly use the menu
                         playerController.setMouseVisible(true);
                         weaponController.toggleSafety(true);
                     }
                 }
                 else
                 {
-                    text.SetActive(false);
+                    interactHelp.SetActive(false);
                 }
             }
             else
             {
-                text.SetActive(false);
+                interactHelp.SetActive(false);
             }
         }
         else
         {
-            text.SetActive(false);
+            interactHelp.SetActive(false);
         }
         if (Input.GetButtonDown("Cancel"))
         {
@@ -53,7 +54,7 @@ public class BuildingManager : MonoBehaviour
         }
     }
 
-    public void QuitBuildingMenu(Turret turret = null)
+    public void QuitBuildingMenu(Turret turret = null) //close Building menu and resume to normal game, if there is a turret to build, tell the buildingBase to build the turret
     {
         if (turret != null)
         {
