86db7ad5f9003a1951ea9cf87ed654bda1617cd3
Alfadas <alfadas.3@gmail.com>
2020-05-26 19:48:58 +0200

    #45 added BombContainerHittable (replaced BombBay)

--- Assets/Scripts/Airships/BombBay.cs
+++ Assets/Scripts/Airships/BombBay.cs
@@ -1,28 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class BombBay : MonoBehaviour
+public class BombBay : AirshipPart
 {
+    [Header("Bombs")]
     [Tooltip("Prefab of the bomb to drop")]
     [SerializeField] Bomb bombPrefab;
+    [SerializeField] int bombCount = 2;
     [Header("Drop point")]
     [Tooltip("Z Point for bomb drop")]
     [SerializeField] int bombDropPoint = -200;
     [Tooltip("-Y distance to ship")]
     [SerializeField] int bombSpawnDistance = 2;
     bool droped = false; //bool to secure one time drop
-
+    Transform[] dropPoints;
+     
+    protected override void Start()
+    {
+        base.Start();
+        List<Transform> transforms = new List<Transform>(GetComponentsInChildren<Transform>());
+        if (transforms.Count == 1)
+        {
+            Debug.LogWarning("No bomb drop points found");
+            return;
+        }
+        transforms.Remove(transform);
+        dropPoints = transforms.ToArray();
+    }
     private void Update()
     {
         if (transform.position.z < bombDropPoint && !droped) // if behind dropPoint and no bomb droped
         {
-            DropBomb();
+            DropBombs();
         }
     }
-    public void DropBomb()
+    public void DropBombs()
     {
         droped = true;
-        Instantiate(bombPrefab, new Vector3(transform.position.x, transform.position.y - bombSpawnDistance, transform.position.z), Quaternion.identity);
+        for(int i = 0; i< bombCount; i += dropPoints.Length)
+        {
+            foreach(Transform dropPoint in dropPoints)
+            {
+                if (i< dropPoints.Length)
+                {
+                    Instantiate(bombPrefab, new Vector3(dropPoint.position.x, dropPoint.position.y - bombSpawnDistance, dropPoint.position.z), Quaternion.identity);
+                }
+            }
+        } 
     }
 }
