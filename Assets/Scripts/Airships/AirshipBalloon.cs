90f85d5a51ae7fdbabcf2e10239c7824b522e383
Alfadas <alfadas.3@gmail.com>
2020-05-31 23:57:48 +0200

    # added Airship Ballon part to let Airships plummet if the ballon is in pre destruction

---
+++ Assets/Scripts/Airships/AirshipBalloon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirshipBalloon : AirshipPart
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void PreDestroyHittable()
+    {
+        Rigidbody mainRigidbody = gameObject.GetComponentInParent<Rigidbody>();
+        mainRigidbody.useGravity = true;
+        base.PreDestroyHittable();
+    }
+}
