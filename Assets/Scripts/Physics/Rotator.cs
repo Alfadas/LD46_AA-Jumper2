2802fc8aede027c06a37ae83b32eef96776eabb4
Alfadas <alfadas.3@gmail.com>
2020-06-08 17:07:07 +0200

    #59 Added Propeller rotation and engine part rename

---
+++ Assets/Scripts/Physics/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] Vector3 rotationSpeed = Vector3.zero;
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + rotationSpeed * Time.deltaTime);
+    }
+}
